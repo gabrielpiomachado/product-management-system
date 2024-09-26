d9434a466b7037e468b70c564ef0e9bf7a680af6
Gabriel Pio Machado <gabrielpiomachado680@gmail.com>
2024-09-25 21:03:35 -0300

    Corrigindo bug de sessão

--- ajax_produtos.php
+++ ajax_produtos.php
@@ -3,7 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require 'includes/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
--- auth/register.php
+++ auth/register.php
@@ -1,6 +1,9 @@
 <?php
 require '../includes/db.php';
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 $error_message = '';
 
